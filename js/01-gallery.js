92460fe6497eab6eaf4c1e3bf008ab4cb8b5ba8d
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-02-27 00:00:33 +0100

    added gallery of images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createImagesMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>
+            `;
+    })
+    .join("");
+}
