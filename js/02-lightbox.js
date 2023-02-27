6b1a7089cbefd8eaf2ccdd072f2346063d307609
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-02-27 13:40:46 +0100

    lightbox done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerElement = document.querySelector(".gallery");
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+galleryContainerElement.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createImagesMarkup(image) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+            `;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
